fc44c87909502b11d356645b3755ac0f0eb596f4
Sanjay053 <sanjayrajan053@gmail.com>
2023-02-28 17:14:37 +0530

    js task 11 modified model

--- javascript/javascript_task11/javascript.js
+++
@@ -1,6 +0,0 @@
-const form = document.querySelector('form');
-const successMessage = document.querySelector('#form');
-form.addEventListener('submitt', (e) => {
-    e.preventDefault();
-    document.querySelector('.alert').style.display = 'block'
-} );
